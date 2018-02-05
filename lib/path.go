7430b0cb3414b51ccdefafa6a6a512edd89089af
Mike Jarmy <mjarmy@gmail.com>
2018-02-05 06:58:41 -0500

    path.filepath.walk()

--- lib/path.go
+++ lib/path.go
@@ -5,6 +5,7 @@
 package lib
 
 import (
+	"os"
 	"path/filepath"
 
 	g "github.com/mjarmy/golem-lang/core"
@@ -40,6 +41,7 @@ func newFilepath() g.Struct {
 
 	stc, err := g.NewStruct([]g.Field{
 		g.NewField("ext", true, ext()),
+		g.NewField("walk", true, walk()),
 	}, true)
 	if err != nil {
 		panic("unreachable")
@@ -60,5 +62,45 @@ func ext() g.NativeFunc {
 			}
 
 			return g.NewStr(filepath.Ext(name.String())), nil
+		})
+}
+
+func walk() g.NativeFunc {
+
+	return g.NewNativeFunc(
+		2, 2,
+		func(cx g.Context, values []g.Value) (g.Value, g.Error) {
+
+			dir, ok := values[0].(g.Str)
+			if !ok {
+				return nil, g.TypeMismatchError("Expected Str")
+			}
+
+			callback, ok := values[1].(g.Func)
+			if !ok {
+				return nil, g.TypeMismatchError("Expected Func")
+			}
+			if callback.MinArity() != 2 || callback.MaxArity() != 2 {
+				return nil, g.ArityMismatchError("2", callback.MinArity())
+			}
+
+			err := filepath.Walk(
+				dir.String(),
+				func(path string, info os.FileInfo, err error) error {
+					if err != nil {
+						return err
+					}
+					_, gerr := callback.Invoke(cx,
+						[]g.Value{g.NewStr(path), newInfo(info)})
+					return gerr
+				})
+
+			if err != nil {
+				if gerr, ok := err.(g.Error); ok {
+					return nil, gerr
+				}
+				return nil, g.NewError("PathError", err.Error())
+			}
+			return g.NullValue, nil
 		})
 }
